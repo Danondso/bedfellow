c3151cd20afb4dec9e82e1cccb4863ebda61029c
Dublin Anondson <stratophone2112@gmail.com>
2022-01-02 21:38:27 -0900

    feat: added types for Home Screen and Details proto: added auth setup for spotify

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
-import {View, Button, Text} from 'react-native';
-import { DETAILS } from '../constants/Screens';
-import {HomeScreenProps} from '../types';
+import { View, Button, Text } from 'react-native';
+import {
+    auth as SpotifyAuth,
+    remote as SpotifyRemote,
+    ApiScope,
+    ApiConfig,
+} from 'react-native-spotify-remote'; 
+import { HomeScreenProps } from '../types';
 
-function HomeScreen({navigation} : HomeScreenProps) {
+const spotifyConfig: ApiConfig = {
+    clientID: process.env.SPOTIFY_CLIENT_ID,
+    redirectURL: process.env.SPOTIFY_REDIRECT_URI,
+    tokenRefreshURL: '',
+    tokenSwapURL: '',
+    scopes: [ApiScope.UserReadEmailScope],
+  };
+
+async function authenticate() {
+    try {
+        const session = await SpotifyAuth.authorize(spotifyConfig);
+        console.log('Session:', session);
+    } catch (error) {
+        console.log('Spotify cannot login', error);
+    }
+}
+
+function HomeScreen({ navigation }: HomeScreenProps) {
   return (
     <View>
       <Text> Home Screen </Text>
       <Button
-        title="Go To Details"
-        onPress={() => navigation.navigate(DETAILS)}
+        title="Log in with Spotify"
+        onPress={() => authenticate()}
       />
     </View>
   );
--- src/screens/types.ts
+++ src/screens/types.ts
@@ -1,10 +1,16 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 export type RootStackParamList = {
-    Home: undefined;
-    Details: undefined;
-  }; 
+  Home: undefined;
+  Details: undefined;
+};
 
-export type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+export type HomeScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'Home'
+>;
 
-export type DetailsScreenProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
+export type DetailsScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'Details'
+>;
