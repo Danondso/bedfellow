4424f127d46636f79d1ed305922fc61cf2ee7470
Dublin Anondson <stratophone2112@gmail.com>
2022-08-07 17:21:34 -0500

    feat: setup authz server so token swaps can occur

--- server/bedfellow-api/src/main.rs
+++ server/bedfellow-api/src/main.rs
@@ -1,27 +1,65 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+use actix_web::{post, App, HttpServer, Responder, HttpResponse};
+use serde::{Serialize, Deserialize};
+use base64;
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ApiResponse {
+    access_token: String,
+    token_type: String,
+    scope: String,
+    expires_in: i32,
+    refresh_token: String
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+#[derive(Serialize, Deserialize)]
+struct ApiRequest {
+    grant_type: String,
+    redirect_uri: String,
+    code: String
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[post("/swap")]
+async fn swap(req_body: String) -> impl Responder {
+    let spotify_client_id="".to_owned();
+    let spotify_client_secret="";
+    let authz_token: String = format!("Basic {}", base64::encode(spotify_client_id + ":" + spotify_client_secret));
+
+    let spotify_client_callback="org.danondso.bedfellow://callback";
+    let _encryption_secret="";
+    let _encryption_method="aes-256-ctr";
+
+
+    let parsed_req_body: Vec<&str> = req_body.split("=").collect();
+
+    let request = ApiRequest { 
+        grant_type: "authorization_code".into(),
+        redirect_uri: spotify_client_callback.into(),
+        code: parsed_req_body[1].into()
+    };
+
+    let client = reqwest::Client::new();
+    // TODO make this unwrap safer
+    let result: ApiResponse = client.post("https://accounts.spotify.com/api/token")
+        .header("Authorization", authz_token)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .form(&request)
+        .send()
+        .await.ok().unwrap().json().await.ok().unwrap();
+
+    debug!("Spotify Token API Result: {:#?}", result);
+    return HttpResponse::Ok().json(result);
 }
 
 
+#[macro_use] extern crate log;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(swap)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Button } from 'react-native';
 import {
   auth as SpotifyAuth,
   remote as SpotifyRemote,
@@ -9,15 +9,17 @@ import {
 import { HomeScreenProps } from '../types';
 
 async function authenticate() {
-  console.log('Do I work?>>?');
   try {
     // Api Config object, replace with your own applications client id and urls
     const spotifyConfig: ApiConfig = {
       clientID: '74632aad45994f23b3b9aabfcb6e4f8b',
-      redirectURL: 'http://localhost:8001',
-      tokenRefreshURL: 'http://localhost:8001/refresh',
-      tokenSwapURL: 'http://localhost:8001/token_swap',
-      scopes: [ApiScope.AppRemoteControlScope, ApiScope.UserFollowReadScope],
+      redirectURL: 'org.danondso.bedfellow://callback', // this should be the app
+      tokenRefreshURL: '',
+      tokenSwapURL: '',
+      scopes: [
+        ApiScope.UserReadCurrentlyPlayingScope,
+        ApiScope.UserFollowReadScope,
+      ],
     };
 
     const session = await SpotifyAuth.authorize(spotifyConfig);
@@ -33,7 +35,6 @@ async function authenticate() {
 function LoginScreen({ navigation }: HomeScreenProps) {
   return (
     <View>
-      <Text> Login Screen </Text>
       <Button title="Log in with Spotify" onPress={() => authenticate()} />
     </View>
   );
