15081d8eaa9d35d3d9cf04f32e364c427a56bbb4
Dublin Anondson <stratophone2112@gmail.com>
2022-08-13 12:35:26 -0500

    fix: tests run with a react-test-renderer devDep bump to 18

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,21 +1,17 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react-native';
-// import App from '../App';
+import React from 'react';
+import { render, screen } from '@testing-library/react-native';
+import AppNavigator from '../src/screens';
 
-// jest.mock('../node_modules/react-native-spotify-remote', () => ({
-//   authorize: jest.fn(),
-// }));
+jest.mock('../node_modules/react-native-spotify-remote', () => ({
+  authorize: jest.fn(),
+}));
 
-// it('Login Screen renders', async () => {
-//   render(<App />);
+it('Login Screen renders', async () => {
+  render(<AppNavigator />);
 
-//   const loginText = await screen.findByText('a smol bean app');
-//   const loginButton = await screen.findAllByText(/Login/);
+  const loginText = await screen.findByText('a smol bean app');
+  const loginButton = await screen.findAllByText(/Login/);
 
-//   expect(loginText).toBeTruthy();
-//   expect(loginButton.length).toBe(1);
-// });
-
-it('asserts true', () => {
-  expect(true).toBeTruthy();
+  expect(loginText).toBeTruthy();
+  expect(loginButton.length).toBe(1);
 });
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 module.exports = {
   preset: '@testing-library/react-native',
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)',
   ],
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src')],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
