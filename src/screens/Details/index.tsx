9bdf1c29d668ebabd7a60661a0367c06a64ba934
Danondso <stratophone2112@gmail.com>
2022-08-16 15:35:52 -0500

    feat: basic music info layout for track

---
+++ src/screens/Details/index.tsx
@@ -0,0 +1,72 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+import { Avatar } from 'react-native-paper';
+import { SpotifyAuthContext } from '../../context';
+import { DetailsScreenProps } from '../../types';
+import styles from './Details.styles';
+import {
+  CurrentPlaybackResponse,
+  TrackObjectFull,
+} from '../../types/spotify-api';
+
+function formatArtistNames(item: TrackObjectFull): string {
+  if (!item) {
+    return 'No Artist Name Available';
+  }
+  if ('artists' in item) {
+    return item.artists.map(artist => artist.name).join(',');
+  }
+  return 'No';
+}
+// TODO once we have cards to click on we'll remove this.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function DetailsScreen({ navigation }: DetailsScreenProps) {
+  const spotifyAuth = useContext(SpotifyAuthContext)?.[0];
+  const [currentSongInfo, setCurrentSongInfo] =
+    useState<CurrentPlaybackResponse>(); // TODO type this
+  useEffect(() => {
+    axios
+      .get<CurrentPlaybackResponse>(
+        'https://api.spotify.com/v1/me/player/currently-playing',
+        {
+          // TODO if this fails we refresh, refactor spotify auth stuff into a hook
+          headers: {
+            Authorization: `Bearer ${spotifyAuth?.accessToken}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+      .then(result => {
+        setCurrentSongInfo(result.data);
+      })
+      .catch(err => console.log(err));
+  }, [spotifyAuth?.accessToken]);
+
+  const item = currentSongInfo?.item as TrackObjectFull;
+  return (
+    <View style={styles.view}>
+      <View style={styles.currentSongView}>
+        {item && 'album' in item ? (
+          <Avatar.Image size={90} source={item?.album.images[0]} />
+        ) : (
+          <Avatar.Text size={90} label={item?.artists[0].name[0]} />
+        )}
+        <Text style={styles.trackName}>
+          {item ? item.name : 'No Track Info Available'}
+        </Text>
+        <Text style={styles.artistName}>
+          {item ? formatArtistNames(item) : 'No Artist Info Available'}
+        </Text>
+        <Text style={styles.albumDescription}>
+          {item ? item.album.name : 'No Album Info Available'}
+        </Text>
+      </View>
+      <View>
+        <Text style={styles.samplesHeading}>Who Sampled?</Text>
+      </View>
+    </View>
+  );
+}
+
+export default DetailsScreen;
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { RootStackParamList } from '../types';
-import DetailsScreen from './Details/Details';
+import DetailsScreen from './Details';
 import { DETAILS, LOGIN } from './constants/Screens';
 import LoginScreen from './Login';
 
@@ -36,7 +36,13 @@ export default function () {
               name={LOGIN}
               component={LoginScreen}
             />
-            <Stack.Screen name={DETAILS} component={DetailsScreen} />
+            <Stack.Screen
+              options={{
+                headerShown: false,
+              }}
+              name={DETAILS}
+              component={DetailsScreen}
+            />
           </Stack.Navigator>
         </NavigationContainer>
       </PaperProvider>
