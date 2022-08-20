bbb4372c3b033c08af73a8bbac1c4494bee283e0
Danondso <stratophone2112@gmail.com>
2022-08-20 10:15:25 -0500

    refactor: call whosampled search endpoint directly with rust

--- server/bedfellow-api/src/controllers/whosampled_controller.rs
+++ server/bedfellow-api/src/controllers/whosampled_controller.rs
@@ -1,7 +1,7 @@
 use actix_web::{get, web, Responder, HttpResponse};
 use serde::{Serialize, Deserialize};
 use std::{process::{Command}};
-
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Track {
@@ -63,23 +63,40 @@ struct WhoSampledSearchResponse {
     tracks: Vec<WhoSampledSearchTrackResult>,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct WhoSampledErrorResponse {
+    error: String,
+}
+
 #[get("/search")]
 pub async fn search(query: web::Query<SearchQuery>) -> impl Responder {
     let query_params: SearchQuery = query.into_inner();
-    let python_output = Command::new("./run_python_search.sh")
-        .arg(query_params.artist)
-        .arg(query_params.track_name)
-        .output();
-    
-    if python_output.is_ok() {
-        let unwrapped_results = python_output.unwrap();
-        let search_results: String = String::from_utf8_lossy(&unwrapped_results.stdout).to_string();
-        let who_sampled_response: Result<WhoSampledSearchResponse, serde_json::Error> = serde_json::from_str(&search_results);
-        if who_sampled_response.is_ok() {
-            return HttpResponse::Ok().json(web::Json(who_sampled_response.unwrap()))
-        } else {
-            info!("{:?}", who_sampled_response.unwrap_err().to_string())
-        }
+ 
+    let client_builder = reqwest::ClientBuilder::new();
+    let client = client_builder
+        .user_agent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36")
+        .cookie_store(true)
+        .use_rustls_tls()
+        .build()
+        .unwrap();
+
+    let timestamp: u128 = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+    let response = client.get("https://www.whosampled.com/ajax/search/")
+        .query(
+            &[("q", format!("{} {}", query_params.artist, query_params.track_name)),
+            ("_", timestamp.to_string())]
+        )
+        .send().await;
+
+    if response.is_ok() {
+        let who_sampled_response: WhoSampledSearchResponse = response.unwrap().json().await.unwrap();
+        return HttpResponse::Ok().json(web::Json(who_sampled_response));
+    } else {
+        
     }
-    return HttpResponse::InternalServerError().json("Unable to parse search results from whosampled scraper script.");
+    let error_response: WhoSampledErrorResponse = { WhoSampledErrorResponse {
+        error: format!("Unable to process request for '{} {}'", query_params.artist, query_params.track_name),
+    } };
+
+    return HttpResponse::InternalServerError().json(web::Json(error_response));
 } 
--- server/bedfellow-api/src/run_python_search.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# this script bridges the call to the scraper script from rust
-# stdout is captured by rust and then returned to the application
-python3 python/search.py $1 $2
