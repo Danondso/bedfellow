4b788ed10d3ecbfb41abd156a13933426e4dd829
Danondso <stratophone2112@gmail.com>
2023-11-30 08:21:14 -0600

    fix: act warnings

--- __tests__/screens/Details/TrackList/index.test.js
+++ __tests__/screens/Details/TrackList/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react-native';
+import { render, screen, waitFor } from '@testing-library/react-native';
 import axios from 'axios';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import TrackList from '../../../../src/screens/Details/TrackList';
@@ -16,35 +16,36 @@ describe('TrackList Test Suite', () => {
     jest.resetAllMocks();
   });
 
-  it('renders loading skeleton when no info is passed in', () => {
-    // This test throws this warning but that's intentional
-    // Warning: An update to TrackList inside a test was not wrapped in act(...).
+  it('renders loading skeleton when no info is passed in', async () => {
     axios.get.mockReturnValueOnce(Promise.resolve({ data: whoSampledPayload }));
+
     render(
       <SafeAreaProvider>
         <TrackList trackInfo={spotifyResponse} />
       </SafeAreaProvider>,
     );
-    expect(screen.getByTestId('tracklist_loading_skeleton')).toBeDefined();
-  });
 
-  it('renders samples data', async () => {
-    axios.get.mockReturnValueOnce(Promise.resolve({ data: whoSampledPayload }));
-    await render(
-      <SafeAreaProvider>
-        <TrackList trackInfo={spotifyResponse} />
-      </SafeAreaProvider>,
+    await waitFor(() =>
+      expect(screen.getByTestId('tracklist_loading_skeleton')).toBeDefined(),
     );
-    expect(screen.getByText('Oasis')).toBeDefined();
+
+    await waitFor(() => {
+      expect(screen.queryByTestId('tracklist_loading_skeleton')).toBeNull();
+      expect(screen.getByText('Oasis')).toBeDefined();
+    });
   });
 
   it('renders empty list component when no sample data is present', async () => {
     axios.get.mockReturnValueOnce(Promise.resolve({ data: emptyPayload }));
-    await render(
+    render(
       <SafeAreaProvider>
         <TrackList trackInfo={spotifyResponse} />
       </SafeAreaProvider>,
     );
-    expect(screen.getByText('No data.')).toBeDefined();
+
+    await waitFor(() => {
+      expect(screen.queryByTestId('tracklist_loading_skeleton')).toBeNull();
+      expect(screen.getByText('No data.')).toBeDefined();
+    });
   });
 });
