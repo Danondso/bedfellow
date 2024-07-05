62c3cd00277801698187aa51df50a6c166a68537
Danondso <stratophone2112@gmail.com>
2024-07-04 20:38:34 -0500

    test: pr feedback

--- __tests__/services/whosampled/utilities/utilities.test.ts
+++ __tests__/services/whosampled/utilities/utilities.test.ts
@@ -3,37 +3,13 @@ import { HEADER_TITLES } from '../../../../src/services/whosampled/enums';
 import singlePage from '../../../fixtures/api/whosampled/sample-single.0';
 import multiPage from '../../../fixtures/api/whosampled/sample-multiple.0';
 import { Sample } from '../../../../src/types/whosampled';
+import sampleResults from '../../../fixtures/api/bedfellow-db-api/sample-info.0';
 
 jest.mock('axios');
 
 describe('WhoSampled Utilities Test Suite', () => {
   it('parses discrete page for matching variant', async () => {
-    const expectedResult: Array<Sample> = [
-      {
-        artist: 'Ponderosa Twins Plus One',
-        image: 'https://www.whosampled.com/static/images/media/track_images_200/lr60124_201393_14349154951.jpg',
-        track: 'Bound',
-        year: 1971,
-      },
-      {
-        artist: 'Brenda Lee',
-        image: 'https://www.whosampled.com/static/images/media/track_images_200/lr9591_2011312_14739796772.jpg',
-        track: "Sweet Nothin's",
-        year: 1959,
-      },
-      {
-        artist: 'Wee',
-        image: 'https://www.whosampled.com/static/images/media/track_images_200/lr28714_2012827_101413805613.jpg',
-        track: 'Aeroplane (Reprise)',
-        year: 1977,
-      },
-      {
-        artist: 'Martin (TV show)',
-        image: 'https://www.whosampled.com/static/images/media/visualmedia_images_200/lr60124_2017215_112028118784.jpg',
-        track: "Jerome's in the House, Watch Your Mouth",
-        year: 1994,
-      },
-    ];
+    const expectedResult: Array<Sample> = sampleResults.samples;
     const document: string = multiPage.toString();
     const result = await parseWhoSampledPage(document, HEADER_TITLES.CONTAINS_SAMPLES);
 
