2664936ea1515ce2a7e553f20f1a909953bf6d2f
Danondso <stratophone2112@gmail.com>
2024-07-19 20:19:33 -0500

    refactor: correct ts errors in ErrorBoundary

--- __tests__/components/helpers/ErrorBoundary.tsx
+++ __tests__/components/helpers/ErrorBoundary.tsx
@@ -1,32 +1,34 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import { Text } from 'react-native';
 
 interface ErrorBoundaryProps {
-  error: unknown;
-  children: unknown;
+  children: JSX.Element;
+}
+interface ErrorBoundaryState {
+  hasError: boolean;
 }
 
-class ErrorBoundary extends React.Component {
-  constructor(props: ErrorBoundaryProps) {
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props) {
     super(props);
     this.state = { hasError: false };
   }
 
   componentDidCatch(error, info) {
     console.log('ERROR:: Failed to render component during test', error, info.componentStack);
+    this.setState({
+      hasError: true,
+    });
   }
 
   render() {
-    // @ts-ignore
     const { hasError } = this.state;
-    // @ts-ignore
     const { children } = this.props;
     if (hasError) {
       return <Text>Failed to render component during test.</Text>;
     }
 
-    return children || <Text>No children to render in ErrorBoundary component.</Text>;
+    return children;
   }
 }
 
