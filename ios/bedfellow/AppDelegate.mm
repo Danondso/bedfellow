fb1ce3f7296806c4ec28a7596ebbb944a6c140ab
Danondso <stratophone2112@gmail.com>
2024-07-20 16:29:38 -0500

    feat: RN 74!

--- ios/bedfellow/AppDelegate.mm
+++ ios/bedfellow/AppDelegate.mm
@@ -65,10 +65,10 @@ continueUserActivity: (nonnull NSUserActivity *)userActivity
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  return [self getBundleURL];
+  return [self bundleURL];
 }
  
-- (NSURL *)getBundleURL
+- (NSURL *)bundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
