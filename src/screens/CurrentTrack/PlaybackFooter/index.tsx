c24540024c1c11e8f9a96e5579dfb061d29eb4c9
Danondso <stratophone2112@gmail.com>
2024-07-29 20:48:57 -0500

    chore: pr feedback

--- src/screens/CurrentTrack/PlaybackFooter/index.tsx
+++ src/screens/CurrentTrack/PlaybackFooter/index.tsx
@@ -2,57 +2,17 @@ import React, { useContext } from 'react';
 import { View, ViewStyle } from 'react-native';
 import { Button } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { AxiosResponse } from 'axios';
 import useSpotifyAPI from '../../../hooks/spotify/useSpotifyAPI';
 import { ImagePaletteContext, ImagePaletteContextData } from '../../../context/ImagePaletteContext';
-import { CurrentPlaybackResponse, UserDevicesResponse } from '../../../types/spotify-api';
+import { CurrentPlaybackResponse } from '../../../types/spotify-api';
 import styles from './PlaybackFooter.styles';
-import { spotifyPOSTData, spotifyGETData, spotifyPUTData } from '../../../services/spotify/SpotifyAPI.service';
-import { AuthResult, SpotifyAuthContext, SpotifyAuthContextData } from '../../../context/SpotifyAuthContext';
+import { performPlaybackAction } from '../../../services/spotify/SpotifyAPI.service';
+import { SpotifyAuthContext, SpotifyAuthContextData } from '../../../context/SpotifyAuthContext';
 
 interface PlayerButtonProps {
   buttonName: string;
   onPress: () => void;
 }
-
-const PLAYER_URL_FRAGMENT = 'v1/me/player';
-
-const performPlaybackAction = async (buttonName: string, spotifyAuth: AuthResult) => {
-  let deviceID;
-  try {
-    const deviceResponse: AxiosResponse<UserDevicesResponse> = await spotifyGETData(
-      `${PLAYER_URL_FRAGMENT}/devices`,
-      spotifyAuth
-    );
-    const { id } = deviceResponse.data.devices?.[0] || '';
-    if (!id) {
-      return;
-    }
-    deviceID = id;
-  } catch (error) {
-    console.log('THIS ERROR', error);
-  }
-  try {
-    switch (buttonName) {
-      case 'forward':
-        await spotifyPOSTData(`${PLAYER_URL_FRAGMENT}/next?device_id=${deviceID}`, spotifyAuth);
-        break;
-      case 'backward':
-        await spotifyPOSTData(`${PLAYER_URL_FRAGMENT}/previous?device_id=${deviceID}`, spotifyAuth);
-        break;
-      case 'pause':
-        await spotifyPUTData(`${PLAYER_URL_FRAGMENT}/pause?device_id=${deviceID}`, spotifyAuth);
-        break;
-      case 'play':
-        await spotifyPUTData(`${PLAYER_URL_FRAGMENT}/play?device_id=${deviceID}`, spotifyAuth);
-        break;
-      default:
-        break;
-    }
-  } catch (err) {
-    console.log(err);
-  }
-};
 
 function PlayerButton({ buttonName, onPress }: PlayerButtonProps) {
   const { imagePalette } = useContext<ImagePaletteContextData>(ImagePaletteContext);
@@ -72,9 +32,6 @@ function PlayerButton({ buttonName, onPress }: PlayerButtonProps) {
 interface PlaybackFooterProps {
   refreshCurrentlyPlayingTrack: () => void;
 }
-
-// TODO, can we just get playback state after performing the action (play/pause/forward/backward) then control the state
-// based on that call in here? OHHH use the useSpotifyAPI hook
 
 function PlaybackFooter({ refreshCurrentlyPlayingTrack }: PlaybackFooterProps) {
   const { spotifyAuth } = useContext<SpotifyAuthContextData>(SpotifyAuthContext);
