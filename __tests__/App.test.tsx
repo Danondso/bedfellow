daba924a6500d6172acb4256c6d076801302e6cd
Danondso <stratophone2112@gmail.com>
2024-09-11 17:22:49 -0500

    feat: better assertions

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react-native';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react-native';
 import { authorize, AuthorizeResult } from 'react-native-app-auth';
 import { Alert, Platform } from 'react-native';
 import axios from 'axios';
@@ -19,6 +19,8 @@ jest.spyOn(Alert, 'alert');
 jest.useFakeTimers();
 jest.mock('axios');
 
+Date.now = () => 1726093188178;
+
 const androidColors: AndroidImageColors = {
   platform: 'android',
   average: defaultPalette.primaryBackground,
@@ -34,6 +36,17 @@ const iosColors: IOSImageColors = {
   secondary: defaultPalette.secondaryBackground,
   detail: defaultPalette.shadow,
   quality: undefined,
+};
+
+const authorizeRequest = {
+  clientId: 'spotify_client_id',
+  redirectUrl: 'com.bedfellow://callback/',
+  scopes: ['user-read-playback-state', 'user-modify-playback-state', 'user-follow-read', 'user-read-currently-playing'],
+  serviceConfiguration: {
+    authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+    tokenEndpoint: 'http://localhost:8085/token',
+  },
+  usePKCE: false,
 };
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -77,16 +90,35 @@ describe('App Test Suite', () => {
 
         mockedImageColors.getColors.mockResolvedValue(androidColors);
 
-        await act(async () => fireEvent.press(await screen.findByText('Login')));
-        await act(async () => fireEvent.press(await screen.findByText('Ponderosa Twins Plus One')));
+        await waitFor(async () => fireEvent.press(await screen.findByText('Login')));
+        await waitFor(async () => fireEvent.press(await screen.findByText('Ponderosa Twins Plus One')));
 
         await waitFor(() => {
           expect(screen.findByText('Ponderosa Twins Plus One')).toBeDefined();
           expect(screen.getByText('Queued Bound by Ponderosa Twins Plus One')).toBeDefined();
-          expect(authorize).toHaveBeenCalledTimes(1);
+          expect(authorize).toHaveBeenCalledWith(authorizeRequest);
           expect(mockedAxios.get).toHaveBeenCalledTimes(3);
+          expect(mockedAxios.get).toHaveBeenNthCalledWith(1, 'https://api.spotify.com/v1/me/player/currently-playing', {
+            headers: { Authorization: 'Bearer ', 'Content-Type': 'application/json' },
+          });
+          expect(mockedAxios.get).toHaveBeenNthCalledWith(
+            2,
+            'http://localhost:8000/api/samples?artist_name=Kanye West&track_name=Bound 2'
+          );
+          expect(mockedAxios.get).toHaveBeenNthCalledWith(
+            3,
+            'https://api.spotify.com/v1/search?q=%2620track%3ABound%2520artist%3APonderosa%2BTwins+Plus+One&type=track&limit=50',
+            { headers: { Authorization: 'Bearer ', 'Content-Type': 'application/json' } }
+          );
+
           expect(mockedImageColors.getColors).toHaveBeenCalledTimes(1);
+
           expect(mockedAxios.post).toHaveBeenCalledTimes(1);
+          expect(mockedAxios.post).toHaveBeenCalledWith(
+            'https://api.spotify.com/v1/me/player/queue?uri=undefined',
+            {},
+            { headers: { Authorization: 'Bearer ', 'Content-Type': 'application/json' } }
+          );
         });
       });
 
@@ -95,15 +127,22 @@ describe('App Test Suite', () => {
 
         mockedImageColors.getColors.mockResolvedValue(androidColors);
 
-        await act(async () => fireEvent.press(await screen.findByText('Login')));
-        await act(async () => fireEvent.press(await screen.findByText('Martin (TV show)')));
+        await waitFor(async () => fireEvent.press(await screen.findByText('Login')));
+        await waitFor(async () => fireEvent.press(await screen.findByText('Martin (TV show)')));
 
         await waitFor(() => {
           expect(screen.findByText('Martin (TV show)')).toBeDefined();
           expect(screen.getByText('Cannot queue tv show')).toBeDefined();
 
-          expect(authorize).toHaveBeenCalledTimes(1);
+          expect(authorize).toHaveBeenCalledWith(authorizeRequest);
           expect(mockedAxios.get).toHaveBeenCalledTimes(2);
+          expect(mockedAxios.get).toHaveBeenNthCalledWith(1, 'https://api.spotify.com/v1/me/player/currently-playing', {
+            headers: { Authorization: 'Bearer ', 'Content-Type': 'application/json' },
+          });
+          expect(mockedAxios.get).toHaveBeenNthCalledWith(
+            2,
+            'http://localhost:8000/api/samples?artist_name=Kanye West&track_name=Bound 2'
+          );
           expect(mockedImageColors.getColors).toHaveBeenCalledTimes(1);
           expect(mockedAxios.post).toHaveBeenCalledTimes(0);
         });
@@ -121,14 +160,29 @@ describe('App Test Suite', () => {
 
         mockedImageColors.getColors.mockResolvedValue(iosColors);
 
-        await act(async () => fireEvent.press(await screen.findByText('Login')));
-        await act(async () => fireEvent.press(await screen.findByText('Ponderosa Twins Plus One')));
+        await waitFor(async () => fireEvent.press(await screen.findByText('Login')));
+        await waitFor(async () => fireEvent.press(await screen.findByText('Ponderosa Twins Plus One')));
 
         await waitFor(() => {
           expect(screen.findByText('Ponderosa Twins Plus One')).toBeDefined();
           expect(screen.getByText('Queued Bound by Ponderosa Twins Plus One')).toBeDefined();
-          expect(authorize).toHaveBeenCalledTimes(1);
-          expect(mockedAxios.get).toHaveBeenCalledTimes(3);
+          expect(authorize).toHaveBeenCalledWith({
+            ...authorizeRequest,
+            redirectUrl: 'org.danondso.bedfellow://callback/',
+          });
+          expect(mockedAxios.get).toHaveBeenNthCalledWith(1, 'https://api.spotify.com/v1/me/player/currently-playing', {
+            headers: { Authorization: 'Bearer ', 'Content-Type': 'application/json' },
+          });
+          expect(mockedAxios.get).toHaveBeenNthCalledWith(
+            2,
+            'http://localhost:8000/api/samples?artist_name=Kanye West&track_name=Bound 2'
+          );
+          expect(mockedAxios.get).toHaveBeenNthCalledWith(
+            3,
+            'https://api.spotify.com/v1/search?q=%2620track%3ABound%2520artist%3APonderosa%2BTwins+Plus+One&type=track&limit=50',
+            { headers: { Authorization: 'Bearer ', 'Content-Type': 'application/json' } }
+          );
+
           expect(mockedImageColors.getColors).toHaveBeenCalledTimes(1);
           expect(mockedAxios.post).toHaveBeenCalledTimes(1);
         });
@@ -148,10 +202,48 @@ describe('App Test Suite', () => {
 
     mockedImageColors.getColors.mockResolvedValue(androidColors);
 
-    await act(async () => fireEvent.press(await screen.getByText('Login')));
-    // TODO check redirectUri
-    expect(authorize).toHaveBeenCalledTimes(1);
+    await waitFor(async () => fireEvent.press(await screen.getByText('Login')));
+    expect(authorize).toHaveBeenCalledWith(authorizeRequest);
     expect(mockedAxios.get).toHaveBeenCalledTimes(9);
+    expect(mockedAxios.get).toHaveBeenNthCalledWith(1, 'https://api.spotify.com/v1/me/player/currently-playing', {
+      headers: { Authorization: 'Bearer ', 'Content-Type': 'application/json' },
+    });
+    expect(mockedAxios.get).toHaveBeenNthCalledWith(
+      2,
+      'http://localhost:8000/api/samples?artist_name=Kanye West&track_name=Bound 2'
+    );
+    expect(mockedAxios.get).toHaveBeenNthCalledWith(
+      3,
+      'https://www.whosampled.com/ajax/search/?q=Kanye West Bound 2&_=1726093188178'
+    );
+    expect(mockedAxios.get).toHaveBeenNthCalledWith(4, 'https://www.whosampled.com/Kanye-West/Bound-2/samples', {
+      responseType: 'text',
+    });
+    expect(mockedAxios.get).toHaveBeenNthCalledWith(
+      5,
+      'https://www.whosampled.com/static/images/media/track_images_200/lr60124_201393_14349154951.jpg',
+      { responseType: 'blob' }
+    );
+    expect(mockedAxios.get).toHaveBeenNthCalledWith(
+      6,
+      'https://www.whosampled.com/static/images/media/track_images_200/lr9591_2011312_14739796772.jpg',
+      { responseType: 'blob' }
+    );
+    expect(mockedAxios.get).toHaveBeenNthCalledWith(
+      7,
+      'https://www.whosampled.com/static/images/media/track_images_200/lr28714_2012827_101413805613.jpg',
+      { responseType: 'blob' }
+    );
+    expect(mockedAxios.get).toHaveBeenNthCalledWith(
+      8,
+      'https://www.whosampled.com/static/images/media/visualmedia_images_200/lr60124_2017215_112028118784.jpg',
+      { responseType: 'blob' }
+    );
+    expect(mockedAxios.get).toHaveBeenNthCalledWith(
+      9,
+      'http://localhost:8000/api/samples?artist_name=Kanye West&track_name=Bound 2'
+    );
+
     expect(mockedImageColors.getColors).toHaveBeenCalledTimes(1);
     expect(screen.getByText('Ponderosa Twins Plus One')).toBeDefined();
   });
@@ -164,13 +256,18 @@ describe('App Test Suite', () => {
         status: 500,
       },
     });
-    await act(async () => fireEvent.press(await screen.findByText('Login')));
-    // TODO check redirectUri
-    expect(authorize).toHaveBeenCalledTimes(1);
+    await waitFor(async () => fireEvent.press(await screen.findByText('Login')));
+    expect(authorize).toHaveBeenCalledWith({
+      ...authorizeRequest,
+      redirectUrl: 'org.danondso.bedfellow://callback/',
+    });
     expect(screen.getByText('Nothing playing currently.')).toBeDefined();
     expect(screen.getByText('No samples found.')).toBeDefined();
     expect(mockedImageColors.getColors).toHaveBeenCalledTimes(0);
     expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+    expect(mockedAxios.get).toHaveBeenLastCalledWith('https://api.spotify.com/v1/me/player/currently-playing', {
+      headers: { Authorization: 'Bearer ', 'Content-Type': 'application/json' },
+    });
   });
 
   describe('Login Failures', () => {
@@ -180,13 +277,13 @@ describe('App Test Suite', () => {
     it('shows login failure alert', async () => {
       // @ts-ignore
       authorize.mockRejectedValueOnce('Failed to login');
-      await act(async () => fireEvent.press(await screen.findByText('Login')));
+      await waitFor(async () => fireEvent.press(await screen.findByText('Login')));
       expect(Alert.alert).toHaveBeenCalledWith('Failed to login');
     });
     it('shows login failure alert with error text', async () => {
       // @ts-ignore
       authorize.mockRejectedValueOnce(new Error('Oops'));
-      await act(async () => fireEvent.press(await screen.findByText('Login')));
+      await waitFor(async () => fireEvent.press(await screen.findByText('Login')));
       expect(Alert.alert).toHaveBeenCalledWith('Failed to login', 'Oops');
     });
   });
