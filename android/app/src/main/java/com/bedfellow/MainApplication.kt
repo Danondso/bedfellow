cb3143cc7d5a2540ee851e7e91f99bdd2c18384d
Danondso <7014871+Danondso@users.noreply.github.com>
2025-08-26 20:42:01 -0500

    fix: android runs

--- android/app/src/main/java/com/bedfellow/MainApplication.kt
+++ android/app/src/main/java/com/bedfellow/MainApplication.kt
@@ -4,11 +4,11 @@ import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeHost 
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
+import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
@@ -33,12 +33,6 @@ class MainApplication : Application(), ReactApplication {
 
     override fun onCreate() {
         super.onCreate()
-        // Initialize SoLoader to load native libraries
-        SoLoader.init(this, /* native exopackage */ false)
-        // Don't load new architecture since it's disabled
-        // if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-        //     // If you opted-in for the New Architecture, we load the native entry point for this app.
-        //     load()
-        // }
+        loadReactNative(this)
     }
 }
